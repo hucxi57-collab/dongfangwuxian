e3a7776c2cbb24d5906bec9d3dc198e09513f8e1
chenyu <hucxi57@gmail.com>
2026-09-12 21:52:11 +0800

    v1.7.5 工具精修04-随机决策图形化

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
  compileSdk = 36
  buildFeatures { buildConfig = true; aidl = true }
  androidResources { additionalParameters += listOf("--no-xml-namespaces", "--no-compile-sdk-metadata") }
- defaultConfig { applicationId = "dfwx.dongdang"; minSdk = 24; targetSdk = 36; versionCode = 1030019; versionName = "1.7.4" }
+ defaultConfig { applicationId = "dfwx.dongdang"; minSdk = 24; targetSdk = 36; versionCode = 1030020; versionName = "1.7.5" }
  flavorDimensions += "catalog"
  productFlavors {
   create("empty") {
